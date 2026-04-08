35a3832560d5b9ca8d3ee9f59f87b6ec11904684
Yalcin Ege Okyar <egeokyar95@gmail.com>
2026-04-08 21:04:31 +0200

    Initial Commit

---
+++ MovieRandomizer/MovieRandomizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRandomizerApp.swift
+//  MovieRandomizer
+//
+//  Created by Ege Okyar on 08.04.26.
+//
+
+import SwiftUI
+
+@main
+struct MovieRandomizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
